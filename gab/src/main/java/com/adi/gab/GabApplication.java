1e15e6faf2d8dd165e2a3018d4a6c0c9526cf700
Cesar Agusto Gomez Perilla <cesar.agomez@supernotariado.gov.co>
2025-04-29 18:53:28 -0500

    Initial Setup

---
+++ gab/src/main/java/com/adi/gab/GabApplication.java
@@ -0,0 +1,13 @@
+package com.adi.gab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabApplication.class, args);
+    }
+
+}
