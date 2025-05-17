b167c56115d5102dc5e6cf13158f93a69faefab3
julicast28 <6jcastanolmc@gmail.com>
2025-05-17 16:08:09 -0500

    Correccion rutas y problemas de visualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 
-import ProfilePage from './pages/ProfilePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import Orders from './pages/OrdersPage';
-import Tracking from './pages/Tracking';
-import Cart from './pages/Cart';
-import Favorites from './pages/Favorites';
-import Returns from './pages/Returns';
 import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
+import AppRoutes from "./routes/AppRoutes.tsx";
 
 function App() {
-  const isAuthenticated = localStorage.getItem('authToken');
 
   return (
-    <Router>
-      <div>
+    <BrowserRouter>
         <Navbar />
-        <Routes>
-          {/* Redirige siempre a /home sin validar authToken */}
-          <Route path="/" element={<Navigate to="/home" />} />
-
-          {/* Rutas normales */}
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tracking" element={<Tracking />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/returns" element={<Returns />} />
-
-          {/* Catch-all: redirige a / */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </div>
-    </Router>
+        <AppRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,31 +1,34 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ProfilePage from './pages/ProfilePage';
-import LoginPage from "./pages/LoginPage";
 import type {JSX} from "react";
-import Orders from './pages/OrdersPage';
-import Tracking from './pages/Tracking';
-import Cart from './pages/Cart';
-import Favorites from './pages/Favorites';
-import Returns from './pages/Returns';
+import HomePage from "../pages/HomePage.tsx";
+import LoginPage from "../pages/LoginPage.tsx";
+import RegisterPage from "../pages/RegisterPage.tsx";
+import ProfilePage from "../pages/ProfilePage.tsx";
+import Tracking from "../pages/Tracking.tsx";
+import Cart from "../pages/Cart.tsx";
+import Favorites from "../pages/Favorites.tsx";
+import Returns from "../pages/Returns.tsx";
+
+
 
 const AppRoutes = (): JSX.Element => {
-    const isAuthenticated = localStorage.getItem('authToken');
-
     return (
         <Routes>
-            {/* Ruta pública */}
-            <Route path="/login" element={<LoginPage />} />  {/* Ruta protegida */}
-            <Route path="/profile" element={isAuthenticated ? ( <ProfilePage />     ) :  <Navigate to="/login" />
-            <Route path="/orders" element={<Orders />} />
+            {/* Redirige siempre a /home sin validar authToken */}
+            <Route path="/" element={<Navigate to="/home" />} />
+
+            {/* Rutas normales */}
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
             <Route path="/tracking" element={<Tracking />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/favorites" element={<Favorites />} />
             <Route path="/returns" element={<Returns />} />
-                    )
-                }
-            />
 
-            {/* Agregar rutas */}
+            {/* Catch-all: redirige a / */}
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 };
