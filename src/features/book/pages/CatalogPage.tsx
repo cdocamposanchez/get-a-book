802a278da1be22057297d1f12f74101e93b42ae8
kbermudezzzz <148507742+kbermudezzzz@users.noreply.github.com>
2025-05-20 10:04:23 -0500

    Update CatalogPage.tsx

--- src/features/book/pages/CatalogPage.tsx
+++ src/features/book/pages/CatalogPage.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import BookCard from "../components/BookCard.tsx";
 import BookFilters from "../components/BookFilters.tsx";
 import { useBooks } from "../hooks/useBook.ts";
 import Spinner from "../../../components/Spinner.tsx";
 
 const CatalogPage = () => {
+    // Estados para filtros
+    const [category, setCategory] = useState<string | null>(null);
+    const [sortOrder, setSortOrder] = useState<'asc' | 'desc' | null>(null);
+    const [maxPrice, setMaxPrice] = useState<number>(400000);
+
     const {
         books,
         loading,
@@ -11,12 +17,16 @@ const CatalogPage = () => {
         page,
         nextPage,
         prevPage,
-    } = useBooks();
+    } = useBooks({ category, sortOrder, maxPrice }); // debes modificar useBooks para que acepte estos filtros
 
     return (
         <div className="flex w-full h-full font-sans">
             <aside className="w-56 bg-green-200 p-5">
-                <BookFilters/>
+                <BookFilters
+                    onCategoryChange={setCategory}
+                    onSortChange={setSortOrder}
+                    onPriceChange={setMaxPrice}
+                />
             </aside>
 
             <div className="flex flex-col flex-1 bg-white p-5 relative">
@@ -24,11 +34,10 @@ const CatalogPage = () => {
                 {error && <p className="mb-2 text-red-600">{error}</p>}
 
                 <div className="overflow-auto mb-20">
-                    {!loading && <BookCard books={books}/>}
+                    {!loading && <BookCard books={books} />}
                 </div>
 
-                <div
-                    className="fixed bottom-0  left-[224px] right-0 bg-white p-5 flex justify-center gap-4 border-t border-gray-300">
+                <div className="fixed bottom-0 left-[224px] right-0 bg-white p-5 flex justify-center gap-4 border-t border-gray-300">
                     <button
                         onClick={prevPage}
                         disabled={page === 0}
@@ -44,7 +53,6 @@ const CatalogPage = () => {
                     </button>
                 </div>
             </div>
-
         </div>
     );
 };
